d84ad4a69dca44152494600b4132a46deb7892d8
0BL1V10N <cvjeticaxel@gmail.com>
2025-05-23 17:00:54 +0200

    chore: add husky for hooks management

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
 import { fileURLToPath } from "node:url";
-import { mergeConfig, defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config";
 
 export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		test: {
-			environment: "jsdom",
-			include: ["tests/**/*.ts"],
-			root: fileURLToPath(new URL("./", import.meta.url)),
-			passWithNoTests: true,
-		},
-	}),
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: "jsdom",
+      include: ["tests/**/*.ts"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+      passWithNoTests: true,
+    },
+  }),
 );
