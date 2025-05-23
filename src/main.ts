e5338d897e5dfdacdc7e6b4e7f2d69c263ab907b
0BL1V10N <cvjeticaxel@gmail.com>
2025-05-23 18:05:25 +0200

    feat: add vue-query and axios

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,37 @@
 import "./assets/main.css";
 
+import {
+  QueryClient,
+  type QueryFunctionContext,
+  VueQueryPlugin,
+} from "@tanstack/vue-query";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
+import { axios } from "@/services";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: async <T>({ queryKey }: QueryFunctionContext): Promise<T> => {
+        const url = queryKey[0];
+        if (typeof url !== "string") {
+          throw new Error("queryKey[0] must be a string URL");
+        }
+
+        const response = await axios.get<T>(url);
+        return response.data;
+      },
+    },
+  },
+});
+
 app.use(createPinia());
+app.use(VueQueryPlugin, { queryClient });
 app.use(router);
 
 app.mount("#app");
---
+++ src/services/axios.ts
@@ -0,0 +1,7 @@
+import Axios from "axios";
+
+export const axios = Axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  responseType: "json",
+  timeout: 20_000,
+});
