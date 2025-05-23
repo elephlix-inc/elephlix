15b917f1d9f38a2fed7e65e864a330cc1e363574
0BL1V10N <cvjeticaxel@gmail.com>
2025-05-23 18:09:33 +0200

    chore: add Codecov bundler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { URL, fileURLToPath } from "node:url";
 
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "elephlix-vue",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
